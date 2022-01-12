d2301a6ab20208527759b54c1fa7a1cc59763521
Tannnkely <tannnkely@gmail.com>
2022-01-12 16:17:56 +0300

    added vendor.js

--- build/js/vendor-closest.js
+++
@@ -1,2 +0,0 @@
-!function(t){t.matches=t.matches||t.mozMatchesSelector||t.msMatchesSelector||t.oMatchesSelector||t.webkitMatchesSelector,t.closest=t.closest||function(t){return this?this.matches(t)?this:this.parentElement?this.parentElement.closest(t):null:null}}(Element.prototype);
-//# sourceMappingURL=vendor-closest.js.map
